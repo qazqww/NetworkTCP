a2cb450863032c04f730cc7e3931fd6ec392b28a
ktm-jg <raptarior@gmail.com>
2019-08-21 11:37:00 +0900

    Initial commit

---
+++ Assets/TCPClient.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+// 프로토콜 : 규칙, 규약
+// 정해진 규칙에 맞도록 제공하는 클래스 : Socket
+// tcp소켓에서 중요한 함수 : Bind, Listen, Poll
+
+/* TCP 클라이언트 동작 순서
+ * 1. 소켓 생성
+ * 2. 서버 연결
+ */
+
+public class TCPClient : MonoBehaviour
+{
+    Socket client;
+    string sendMsg = string.Empty;
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0, 0, 300, 300), "Connect"))
+        {
+            Connect("127.0.0.1", 80);
+        }
+
+        sendMsg = GUI.TextField(new Rect(0, 300, 300, 300), sendMsg);
+
+        if (GUI.Button(new Rect(0, 600, 300, 300), "Send"))
+        {
+            if (!sendMsg.Equals(string.Empty))
+            {
+                byte[] msg = System.Text.Encoding.UTF8.GetBytes(sendMsg); // Unicode -> UTF8 -> Byte
+                client.Send(msg);
+                sendMsg = string.Empty;
+            }
+        }
+    }
+
+    bool Connect(string ipaddress, int port)
+    {
+        try
+        {
+            // 소켓 생성
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // 서버 연결
+            // ※ EndPoint 클래스는 ip address와 port를 관리하는 클래스
+            client.Connect(ipaddress, port);
+
+            return true;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex);
+            client = null;
+        }
+
+        return false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        client.Shutdown(SocketShutdown.Both); // 소켓을 종료. 양쪽 다
+        client.Close(); // 연결을 닫음
+        client = null;
+    }
+}
---
+++ Assets/TCPServer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class TCPServer : MonoBehaviour
+{
+    Socket server;
+    Socket client; // 클라이언트 소켓 복사본
+
+    void Start()
+    {
+        CreateServer();
+    }
+
+    void Update()
+    {
+        // 소켓의 상태 확인
+        // 대기 시간, 통신모드 (SelectRead: 읽어들일 데이터가 있는지)
+        if(server.Poll(0, SelectMode.SelectRead))
+        {
+            client = server.Accept();
+        }
+
+        if (client != null && client.Poll(0, SelectMode.SelectRead))
+        {
+            byte[] buffer = new byte[1024];
+            int recvLength = client.Receive(buffer); // 사이즈 값을 int 타입으로 리턴
+            if (recvLength == 0) // 클라이언트가 종료된 경우
+            {
+                client = null;
+                return;
+            }
+            else
+            {
+                client.Send(buffer);
+            }
+
+            Debug.Log(buffer);
+        }
+    }
+
+    void CreateServer()
+    {
+        try
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(IPAddress.Any, 80));
+            server.Listen(1); // 접속 가능 대수를 int 타입으로 받음
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if(client != null)
+        {
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+            client = null;
+        }
+        if (server != null)
+        {
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+            server = null;
+        }
+    }
+}
